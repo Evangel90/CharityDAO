f9b9d0cf9f78dba3983be07bb84c66f085a9c96e
Evangel90 <oladipoevangel90@gmail.com>
2025-03-01 10:55:13 +0100

    removed the setDAO cause Ownable.sol handles it's funcitonality already

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
@@ -9,12 +10,6 @@ contract Treasury is Ownable {
     using Address for address payable;
     event DonatedEther(address to, uint256 amount);
     event Received(address from, uint256 amount);
-
-    //Function to transfer ownership of Treasury to a DAO
-    function setDAO(address _dao) external onlyOwner {
-        require(_dao != address(0), "Invalid address");
-        transferOwnership(_dao);
-    }
 
     // Function to donate Ether to an owner address
     function donateEther(
